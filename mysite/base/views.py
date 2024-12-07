c2588edf98ef57eaea16b44fcb92c5e5c0a63d58
pankaj-bansal00 <pnkajbansal9900@gmail.com>
2024-12-07 06:34:38 +0530

    register basics

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect
 
 # Create your views here.
+
 def login(request):
+    
+    
     return render(request,"register/login.html")
 
 def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        else:
+            form = UserCreationForm()
+
     return render(request, "register/signup.html")
